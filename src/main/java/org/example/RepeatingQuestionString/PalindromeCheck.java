f2f0fd0b8266960087a2862e15e71ec9a7a9fb8c
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-07-12 13:41:26 +0530

    Create PalindromeCheck.java

---
+++ src/main/java/org/example/RepeatingQuestionString/PalindromeCheck.java
@@ -0,0 +1,16 @@
+package org.example.RepeatingQuestionString;
+
+public class PalindromeCheck {
+   public static void main(String[] args) {
+        String str = "Madam";
+       // Convert to lowercase and remove non-alphanumeric characters (optional)
+       String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+       if(cleaned.contentEquals(new StringBuilder(cleaned).reverse())){
+           System.out.println("Palindrome");
+       }else {
+           System.out.println("Not Palindrome");
+       }
+
+    }
+}
