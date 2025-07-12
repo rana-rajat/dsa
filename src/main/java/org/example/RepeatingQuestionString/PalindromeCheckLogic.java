5f0be01def10642cada05aab538fad40c43c2c6f
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-07-12 16:45:47 +0530

    Create PalindromeCheckLogic.java

---
+++ src/main/java/org/example/RepeatingQuestionString/PalindromeCheckLogic.java
@@ -0,0 +1,34 @@
+package org.example.RepeatingQuestionString;
+
+public class PalindromeCheckLogic {
+    public static void main(String[] args) {
+        String str = "Mafdam";
+        String clean = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        char[] newString = clean.toCharArray();
+        System.out.println(newString);
+        int start = 0;
+        int end = newString.length-1;
+//
+//        StringBuilder reversedBuilder = new StringBuilder();
+//        for (int i = clean.length() - 1; i >= 0; i--) {
+//            reversedBuilder.append(clean.charAt(i));
+//        }
+//
+//        String reversed = reversedBuilder.toString();
+        while(start<end){
+            char temp = newString[start];
+            newString[start]=newString[end];
+            newString[end]=temp;
+            start++;
+            end--;
+        }
+        String reversed = new String(newString);
+        System.out.println(reversed);
+        if(reversed.equals(clean)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
