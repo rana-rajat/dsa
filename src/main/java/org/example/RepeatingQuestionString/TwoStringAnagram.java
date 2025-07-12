9c400d1dfc23457194670b3d7d4b97ecf322148d
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-07-12 16:53:30 +0530

    Create TwoStringAnagram.java

---
+++ src/main/java/org/example/RepeatingQuestionString/TwoStringAnagram.java
@@ -0,0 +1,30 @@
+package org.example.RepeatingQuestionString;
+
+import java.util.Arrays;
+
+public class TwoStringAnagram {
+    public static void main(String[] args) {
+        String str1 = "Listen";
+        String str2 = "Silent";
+
+        String s1 = str1.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String s2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Anagram");
+        } else {
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("Anagrams");
+            } else {
+                System.out.println("Not Anagrams");
+            }
+
+        }
+
+    }
+}
