9f17f669615c70d27a05f26f1da22ea88eae96a2
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-11-15 02:09:59 +0530

    Create SixthSolidRhombus.java

---
+++ src/main/java/ThirdAdvancePattern/SixthSolidRhombus.java
@@ -0,0 +1,17 @@
+package ThirdAdvancePattern;
+
+public class SixthSolidRhombus {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
