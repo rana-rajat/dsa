2978000033f5b90cc72406075b2fe971ea44de3a
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-11-18 00:02:24 +0530

    Update SeventhHollowRhombus.java

--- src/main/java/ThirdAdvancePattern/SeventhHollowRhombus.java
+++ src/main/java/ThirdAdvancePattern/SeventhHollowRhombus.java
@@ -4,13 +4,13 @@ public class SeventhHollowRhombus {
     public static void main(String[] args) {
         for(int i = 1 ; i <=5 ; i++){
             for(int j = 5; j >= i; j--){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for(int j =1 ; j<=5; j++){
                 if(i ==1 || j==1 || j==5 || i==5){
                     System.out.print("* ");
                 }else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
