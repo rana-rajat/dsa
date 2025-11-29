f3aa42cc29cbe39613b5b59873ac7b1e654258a2
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-11-29 18:59:27 +0530

    Create ThirdBinarySearch.java

---
+++ src/main/java/FourthArray/ThirdBinarySearch.java
@@ -0,0 +1,27 @@
+package FourthArray;
+
+public class ThirdBinarySearch {
+    public static int binarySearch(int[] numbers, int key) {
+        int start = 0, end = numbers.length - 1;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] < key) {
+                start = mid + 1; //right
+            } else {//left
+                end = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 4;
+        System.out.println("index for key is "+binarySearch(arr, key));
+    }
+}
