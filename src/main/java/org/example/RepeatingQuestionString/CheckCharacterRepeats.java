e71833925af3d36d8cc4c60ab66ca978d4bb40dc
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2026-01-05 07:40:13 +0530

    Update CheckCharacterRepeats.java

--- src/main/java/org/example/RepeatingQuestionString/CheckCharacterRepeats.java
+++ src/main/java/org/example/RepeatingQuestionString/CheckCharacterRepeats.java
@@ -1,16 +1,28 @@
 package org.example.RepeatingQuestionString;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CheckCharacterRepeats {
     public static void main(String[] args) {
 
         String str = "abcadfegasdx";
         boolean[] seen = new boolean[256];
 
-        for (char ch : str.toCharArray()){
-            if(!seen[ch]){
-                seen[ch]= true;
-            }else{
-                System.out.println(ch+" is Repeating ");
+        for (char ch : str.toCharArray()) {
+            if (!seen[ch]) {
+                seen[ch] = true;
+            } else {
+                System.out.println(ch + " is Repeating ");
+            }
+        }
+        //Also my way
+        Set<Character> newStr = new HashSet<>();
+        for (char ch : str.toCharArray()) {
+            if (newStr.contains(ch)) {
+                System.out.println(ch + " is Repeating ");
+            } else {
+                newStr.add(ch);
             }
         }
 
